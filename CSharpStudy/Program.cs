2df6abc2144c211c1c537d39f3bda104c655ff31
Lee dohyun <12423098+riviea@users.noreply.github.com>
2023-09-06 09:19:53 +0900

    Modify Program.cs
    Solution 파일 분할

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -8,25 +8,9 @@ namespace CSharpStudy
 {
     internal class Program
     {
-
-        static public long solution(long n)
-        {
-            long sqrt = (long)Math.Sqrt(n);
-            long answer = sqrt * sqrt;
-            return answer == n ? (sqrt + 1) * (sqrt + 1) : -1;
-        }
-
-        static public long solution10(long n)
-        {
-            string temp = n.ToString();
-
-            long answer = long.Parse(temp.OrderByDescending(x=>x).ToArray());
-            return answer;
-        }
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}", solution10(121345));
+            Console.WriteLine("{0}", Solution.solution11(11));
         }
     }
 }
---
+++ CSharpStudy/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy
+{
+    internal class Solution
+    {
+        static public long solution(long n)
+        {
+            long sqrt = (long)Math.Sqrt(n);
+            long answer = sqrt * sqrt;
+            return answer == n ? (sqrt + 1) * (sqrt + 1) : -1;
+        }
+
+        static public long solution10(long n)
+        {
+            string temp = n.ToString();
+
+            long answer = long.Parse(temp.OrderByDescending(x => x).ToArray());
+            return answer;
+        }
+
+        static public bool solution11(int x)
+        {
+            string str = x.ToString();
+
+            int total = 0;
+            foreach (char temp in str)
+            {
+                total += (int)(temp - '0');
+            }
+
+            Console.WriteLine(total);
+
+            if ((x % total) == 0)
+                return true;
+            return false;
+        }
+    }
+}
